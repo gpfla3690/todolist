40b96badf4a1111f2967b08a5d692131353f8514
gpfla3690 <gpfla3690@gmail.com>
2022-05-01 16:50:08 +0900

    게시글 생성 및 리스트 기능 구현

--- article/forms.py
+++ article/forms.py
@@ -7,3 +7,10 @@ class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ['todo']
+
+        labels = {
+            'todo': '',
+        }
+        widgets = {
+            'todo': forms.TextInput(attrs={'placeholder': '할일을 입력해 주세요.'})
+        }
